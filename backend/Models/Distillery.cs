5f199b98b4754686a297182115640f34038dcfb0
shashikumarvijayan <111286827+shashikumarvijayan@users.noreply.github.com>
2025-07-30 11:53:48 +0800

    Delete backend directory

--- backend/Models/Distillery.cs
+++
@@ -1,9 +0,0 @@
-namespace TasteWhiskyApi.Models;
-
-public class Distillery
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = "";
-    public string Region { get; set; } = "";
-    public ICollection<Dram> Drams { get; set; } = new List<Dram>();
-}
--- backend/Models/User.cs
+++
@@ -1,8 +0,0 @@
-namespace TasteWhiskyApi.Models;
-
-public class User
-{
-    public int Id { get; set; }
-    public string UserName { get; set; } = "";
-    public ICollection<TastingNote> Notes { get; set; } = new List<TastingNote>();
-}
